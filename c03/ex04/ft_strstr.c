ef9c43bc21852cd4994ae4069f663baae8b27361
Pedro Gil Leitao Silva Pinhao <psilva-p@c1r7s8.42lisboa.com>
2025-08-08 13:50:23 +0100

    a

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: psilva-p <psilva-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 14:08:39 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/08/07 20:03:55 by psilva-p         ###   ########.fr       */
+/*   Updated: 2025/08/08 13:48:06 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,32 @@
 char	*ft_strstr(char *str, char *to_find)
 {
 	char	*temp_ptr_str;
+	char	*temp_ptr_tofind;
 
 	if (*to_find == '\0')
 		return (str);
 	while (*str)
 	{
-		if (*str == *to_find )
+		temp_ptr_str = str;
+		temp_ptr_tofind = to_find;
+		while (*str == *to_find && *str && *to_find)
 		{
-			temp_ptr_str = str;
-			while (*str == *to_find && *str && *to_find)
-			{
-				str++;
-				to_find++;
-			}
+			str++;
+			to_find++;
 		}
+		temp_ptr_tofind = to_find;
+		if (*to_find == '\0')
+			return (str);
 		str++;
 	}
-	return (str);
+	return (0);
 }
 
 int main()
 {
-	char s1[] = "ola mundubo";
-	char s2[] = "ub";
+	char s1[] = "abcde";
+	char s2[] = "cd";
 
-	//printf("%s\n", ft_strstr(s1, s2));
-	printf("%s\n", strstr(s1, s2));
+	printf("%s\n", ft_strstr(s1, s2));
+	//printf("%s\n", strstr(s1, s2));
 }
