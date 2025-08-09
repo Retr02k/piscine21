e987762004098579e42baa1379d2bbae83b4a084
Pedro Gil Leitao Silva Pinhao <psilva-p@c1r9s8.42lisboa.com>
2025-08-09 14:01:15 +0100

    a

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: psilva-p <psilva-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 14:08:39 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/08/08 13:48:06 by psilva-p         ###   ########.fr       */
+/*   Updated: 2025/08/09 13:57:44 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,22 @@ char	*ft_strstr(char *str, char *to_find)
 			str++;
 			to_find++;
 		}
-		temp_ptr_tofind = to_find;
 		if (*to_find == '\0')
 			return (str);
-		str++;
+		to_find = temp_ptr_tofind;
+		str = temp_ptr_str + 1;
 	}
 	return (0);
 }
 
 int main()
 {
-	char s1[] = "abcde";
-	char s2[] = "cd";
+	//char s1[] = "hello world";
+	//char s2[] = "world";
 
-	printf("%s\n", ft_strstr(s1, s2));
+	//printf("%s\n", ft_strstr(s1, s2));
 	//printf("%s\n", strstr(s1, s2));
+	printf("%s\n", ft_strstr("hello world", "world"));	// should print "world"
+	printf("%s\n", ft_strstr("abcabcabcd", "abcd"));	// should print "abcd"
+	printf("%s\n", ft_strstr("abcdef", "gh"));			// should print (null)
 }
